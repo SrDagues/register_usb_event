2b636a45913e91d89b3f6199de5c0cf1be3671d8
SrDagues <danicoin@gmail.com>
2022-01-06 12:15:29 +0100

    Create script.ps1
    Example Register-WmiEvent

---
+++ script.ps1
@@ -0,0 +1,41 @@
+function Enable-DriveTrace {
+    [CmdletBinding()]param()
+    Write-Host "Creo evento"
+    $query = "Select * from __InstanceCreationEvent within 1 where TargetInstance ISA 'Win32_LogicalDisk'"
+    $identifier = "DeviceConnected" # el nombre que yo quiera
+    $actionBlock = {
+        Write-Host "UBS INSERTADO"
+        Write-Host $event
+        $e = $event.SourceEventArgs.NewEvent.TargetInstance # qué eventos se han producido
+        $partition = "ASSOCIATORS OF {Win32_LogicalDisk.DeviceID='$($e.DeviceID)'} WHERE AssocClass = Win32_LogicalDiskToPartition"
+        $p = Get-WmiObject -Query $partition
+        $disk = "ASSOCIATORS OF {Win32_LogicalDisk.DeviceID='$($e.DeviceID)'} WHERE AssocClass = Win32_DiskDriveToDiskPartition"
+        $d = Get-WmiObject -Query $disk
+        $keypath = "HKLM:\SYSTEM\CurrentControlSet\Enum\$($d.PNPDeviceID)\Device Parameters\Partmgr"
+        $DiskId = (Get-ItemProperty -Path $keypath).diskid
+        $output = @()
+        $props = [ordered]@{
+            DiskID = $DiskId
+            Partitions = $d.partitions
+            PartititonIndex = $p.Index
+            DriveLetter = $e.DeviceID
+            Size = $e.size
+            FreeSpace = $e.FreeSpace
+            FileSystem = $e.fileSystem
+            HostName = $e.systemname
+        }
+        $outPut = New-Object psobject -Property $props
+        Write-Host $output
+        $outPutProps = $output | Select-Object HostName, DiskID ,FileSystem, DriveLetter, @{N='FreeSpace';E={"$([math]::Round($(($_.FreeSpace)/1GB),2)) GB"}}
+
+        foreach($pr in $outPutProps.psobject.Properties.name){
+            Write-Host -ForegroundColor Green "$($pr)$(" " * $(20-$pr.length)) : $($outPutProps.$pr)"
+        }
+
+    } 
+    Register-WmiEvent -Query $query -SourceIdentifier $Identifier -Action $actionBlock
+}
+
+$identifier = @(Enable-DriveTrace)[-1]
+
+#Unregister-Event -SourceIdentifier $identifier -Force
